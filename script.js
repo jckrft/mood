f4fda389bf509f7d8e52c054986bed97a4c6c032
jckrft <jckrft@gmail.com>
2021-10-03 14:27:07 -0400

    event listener

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const fetchMETData = () => {
-  const metAPIUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects';
+const fetchMETData = (objectID) => {
+  const metAPIUrl = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`;
 
+  console.log('request');
+  console.log(metAPIUrl);
+  
   fetch(metAPIUrl)
       .then((res) => { return res.json() })
       .then((resJSON) => {
@@ -12,4 +15,17 @@ const fetchMETData = () => {
       })
 }
 
-fetchMETData()
+const button = document.querySelector('#get-artwork');
+button.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  const moodOptions = document.querySelector('#select-mood').value;
+  const objectURL = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${moodOptions}`
+  
+  fetchMETData("782307");
+});
+
+
+
+
+
