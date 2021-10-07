83f4627582b04d02e68ed1f614e79cf56d07d3bd
jckrft <jckrft@gmail.com>
2021-10-07 17:44:19 -0400

    or not

--- script.js
+++ script.js
@@ -18,24 +18,24 @@
   options show met data
 */
 
-const word = "despondent"
-const fetchMERData = (word) => {
+// const word = "despondent"
+// const fetchMERData = (word) => {
   
-  const API_KEY = '9beda46b-4c74-4563-bcd0-a24cac9ad8c2';
-  const base = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/?key=9beda46b-4c74-4563-bcd0-a24cac9ad8c2'
+//   const API_KEY = '9beda46b-4c74-4563-bcd0-a24cac9ad8c2';
+//   const base = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/?key=9beda46b-4c74-4563-bcd0-a24cac9ad8c2'
 
-  const dicURl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${API_KEY}`
-    console.log(dicURl)
+//   const dicURl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${API_KEY}`
+//     console.log(dicURl)
     
-  fetch(dicURl)
-    .then((results) => {
-      return results.json()
-    })
-    .then((resultsJSON) => {
-      console.log(resultsJSON)
-      showDefinition(resultsJSON)
-    })
-}
+//   fetch(dicURl)
+//     .then((results) => {
+//       return results.json()
+//     })
+//     .then((resultsJSON) => {
+//       console.log(resultsJSON)
+//       showDefinition(resultsJSON)
+//     })
+// }
 
   const fetchMETData = (objectID) => {
     const metAPIUrl = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`;
@@ -77,7 +77,7 @@ const fetchMERData = (word) => {
     artistName.innerText = metData.artistDisplayName
     title.innerText = metData.title
     date.innerText = metData.objectDate
-    metDataDiv.append(artistName, title, date)
+    metDataDiv.append(title, artistName, date)
   
   }
     
@@ -85,11 +85,11 @@ const fetchMERData = (word) => {
   const showMoodDescription = (moodOptions) => {
     const moodDescriptions =
     {
-      "782307": "When you're feeling the opposite of surprised, or: an alternative to deer caught in headlights",
+      "782307": "When you're feeling the opposite of surprised, or: the aftermath of a deer caught in headlights",
       "436575": "A sky painted as dark as your feelings",
       "369627": "Is this hand real? Is it mine... or yours?",
       "36065": "You're sleepy, you sleep. A man with his priorities straight.",
-      "399140": "A woman alone in a field, it's both literal and metaphorical",
+      "399140": "A woman alone in a field. It's both literal and metaphorical.",
       "436041": "In the story of Samson and Delilah, Delilah convinces Samson to share why he possesses 'God like' strength, when he reveals that it is actually due to his hair, she waits until he falls asleep and cuts it off. The Philistines come and are able to successfully kill him now that he is without strength. Malice, indeed.",
       "435876": "Madame Cézanne was the artis's wife. This is  obviously the look of a woman who does not believe the story of why that other woman is in your DMs",
     }
@@ -101,18 +101,18 @@ const fetchMERData = (word) => {
   }
 
 
-  const showDefinition = (word) => {
-    // const definitions =
-    // {
-    //   "782307": "despondent"
-    // }
+  // const showDefinition = (word) => {
+  //   // const definitions =
+  //   // {
+  //   //   "782307": "despondent"
+  //   // }
   
-    const moodDefinitionDiv = document.querySelector('#definition')
-    moodDefinitionDiv.innerHTML = ''
-    const moodDefinition = document.createElement('p');
-    moodDefinition.innerText = word[0].shortdef
-    moodDefinitionDiv.append(moodDefinition)
-  }
+  //   const moodDefinitionDiv = document.querySelector('#definition')
+  //   moodDefinitionDiv.innerHTML = ''
+  //   const moodDefinition = document.createElement('p');
+  //   moodDefinition.innerText = word[0].shortdef
+  //   moodDefinitionDiv.append(moodDefinition)
+  // }
 
 
   const button = document.querySelector('#get-artwork');
@@ -121,11 +121,11 @@ const fetchMERData = (word) => {
 
     const moodOptions = document.querySelector('#select-mood').value;
     
-    fetchMERData(word)
+    // fetchMERData(word)
     // fetchMERData(moodOptions);
     fetchMETData(moodOptions);
     // fetchMERData(moodOptions);
-    showDefinition(word)
+    // showDefinition(word)
     showMoodDescription(moodOptions);
   });
 
